6b68dc75505ec2128e242bd86fe376dbf1ff52a2
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-04-03 14:56:24 +0200

    replace sticky to fixed

--- styles/ChatContentElements.js
+++ styles/ChatContentElements.js
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 export const Container = styled.div``
 
 export const Header = styled.div`
-  position: sticky;
+  position: fixed;
+  width: 100%;
   top: 0;
   background-color: white;
   z-index: 5;
