dbef2101982915ab19e93b571442489a959e1677
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-04-05 12:34:19 +0200

    set max height container to avoid issue at some mobile browsers

--- styles/ChatElements.js
+++ styles/ChatElements.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  max-height: 100vh;
+  overflow: hidden;
 `
 
 export const ChatContainer = styled.div`
